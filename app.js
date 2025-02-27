88504a2b5fd8bf616271f851567cee276e6ef454
Gabby-Moon <194023457+Gabby-Moon@users.noreply.github.com>
2025-02-27 13:41:40 -0800

    nodeifyed the project

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running http://localhost:${PORT}`);
+});
