7ace930093a0ec62f15f5c043d4815ee4dbd5943
VibeSyntax <138536097+beza102@users.noreply.github.com>
2025-02-27 14:11:59 -0800

    Added a thankyou page and playlist page

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 import express from "express";
 
+//Instantiate an Express application
 const app = express();
+
+
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 
 const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+app.post('/thankyou', (req,res)=>{
+    res.render('thankyou');
+});
+app.get('/admin', (req,res)=>{
+    res.render('playlists');
 });
 
 app.listen(PORT, () => {
